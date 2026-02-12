497964c551e94974f903de2ae7ac657d11eea3c0
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2026-02-12 19:07:27 -0300

    Adding route for getting all events for public. Creating dashboard for Public

--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   createEvent,
   deleteEvent,
+  getAllEvents,
   getEventById,
   getMyEvents,
   updateEvent,
@@ -10,11 +11,14 @@ import { protect } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
-// Routes for event management
+// Routes for event management (private)
 router.post('/', protect, createEvent);
 router.get('/my-events', protect, getMyEvents);
 router.delete('/:id', protect, deleteEvent);
 router.get('/:id', protect, getEventById);
 router.put('/:id', protect, updateEvent);
+
+// Public route to get all events
+router.get('/', getAllEvents);
 
 export default router;

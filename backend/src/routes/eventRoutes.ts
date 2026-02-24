d2e0e0d705a6607b6bcbacc175b41b426ed0447c
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2026-02-23 23:24:05 -0300

    Modifying event routes by adding cloudinary and protect mid

--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -13,26 +13,10 @@ import uploadCloud from '../config/cloudinary';
 const router = express.Router();
 
 // Routes for event management (private)
-router.post(
-  '/',
-  protect,
-  (req, res, next) => {
-    uploadCloud.single('image')(req, res, (err) => {
-      if (err) {
-        console.error(err);
-        return res
-          .status(500)
-          .json({ message: 'Error en la subida', detail: err.message });
-      }
-      next();
-    });
-  },
-  createEvent
-);
+router.post('/', protect, uploadCloud.single('image'), createEvent);
 router.get('/my-events', protect, getMyEvents);
 router.delete('/:id', protect, deleteEvent);
-router.put('/:id', protect, updateEvent);
-
+router.put('/:id', protect, uploadCloud.single('image'), updateEvent);
 // Public route to get events
 router.get('/', getAllEvents);
 router.get('/:id', getEventById);

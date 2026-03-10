a7afa1ed112a4a70bf76c9682d4322a86f23d5c3
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2026-03-10 16:55:11 -0300

    Input tests added

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -3,28 +3,24 @@ import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
 
-// 1. Configuración de Cloudinary
+// Configuración de Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// 2. Configuración del Storage
+// Configuración del Storage
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: async (req, file) => {
     return {
       folder: 'events_app',
-      allowed_formats: ['jpg', 'png', 'jpeg'],
-      // Quitamos el public_id por ahora para que Cloudinary lo genere solo
-      // y descartar errores ahí
+      allowed_formats: ['jpg', 'png', 'jpeg']
     };
   },
 });
 
-// 3. Crear el middleware
-// Agregamos una función de filtro para ver si Multer llega a este punto
 const uploadCloud = multer({ 
   storage: storage,
   fileFilter: (req, file, cb) => {
